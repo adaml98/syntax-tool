1908d76af0f99702dff5b357ea9b8d2df0dc6b77
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-07-14 09:58:10 +0100

    implement psql cli commands

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+#!/usr/bin/env node
+const { Command } = require("commander");
+const { description, version } = require("./package.json");
+
+const program = new Command();
+
+program
+  .name("syntax-tool")
+  .description(description)
+  .version(version, "-v", "-version");
+
+program
+  .command("psql")
+  .description("psql cli commands")
+  .action(() => {
+    console.log(`
+\\l          - show databases
+\\c db_name  - connect to db
+\\dt         - show tables
+\\dt+        - show tables and more info
+\\d tb_name  - display more information about a specific table in a database
+\\?          - show other commands
+\\q          - exit CLI
+:q          - Return to psql cli from database/table/query view
+`);
+  });
+
+program.parse();
