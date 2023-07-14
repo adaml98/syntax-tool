dcf0628de18d1e8ce1c572bd5569142d598a8da7
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-07-14 10:32:44 +0100

    clean up results

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
 const { description, version } = require("./package.json");
-
+const chalk = require("chalk");
 const program = new Command();
 
 program
@@ -13,15 +13,20 @@ program
   .command("psql")
   .description("psql cli commands")
   .action(() => {
+    console.log(`${chalk.bgCyanBright(" PSQL CLI COMMANDS ")}`);
     console.log(`
-\\l          - show databases
-\\c db_name  - connect to db
-\\dt         - show tables
-\\dt+        - show tables and more info
-\\d tb_name  - display more information about a specific table in a database
-\\?          - show other commands
-\\q          - exit CLI
-:q          - Return to psql cli from database/table/query view
+${chalk.greenBright("\\l")}          - show databases
+${chalk.greenBright("\\c db_name")}  - connect to db
+${chalk.greenBright("\\dt")}         - show tables
+${chalk.greenBright("\\dt+")}        - show tables and more info
+${chalk.greenBright(
+  "\\d tb_name"
+)}  - display more information about a specific table in a database
+${chalk.greenBright("\\?")}          - show other commands
+${chalk.greenBright("\\q")}          - exit CLI
+${chalk.greenBright(
+  ":q"
+)}          - Return to psql cli from database/table/query view
 `);
   });
 
